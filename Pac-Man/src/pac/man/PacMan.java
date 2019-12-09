581cbe94ff074e09cae9ecccdbbb2f76a318d5fb
Aldrich <aldrich.lay@gmail.com>
2019-12-09 12:28:07 -0400

    asdfgh

--- Pac-Man/src/pac/man/PacMan.java
+++ Pac-Man/src/pac/man/PacMan.java
@@ -5,15 +5,10 @@
  */
 package pac.man;
 
-/**
- *
- * @author Mati & Ferb
- */
+
 public class PacMan {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
