bddf2ba66b15417a2a63bfd51e71dac256ea478d
Aldrich <aldrich.lay@gmail.com>
2019-12-14 19:10:28 -0400

    Laberinto

--- Pac-Man/src/pac/man/Laberinto.java
+++ Pac-Man/src/pac/man/Laberinto.java
@@ -1,7 +1,12 @@
 
 package pac.man;
 
-
-public class Laberinto {
-    public Celda[][];
+public class Laberinto
+{
+    public Celda[][] Laberinto;
+    
+    public void crear()
+    {
+        Laberinto = new Celda[3][3];
+    }
 }
